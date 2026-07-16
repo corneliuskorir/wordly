b9542651cfd551e947eca99eaa9aba38691589ac
Cornelius Korir <cornelius.korir@student.moringaschool.com>
2026-07-16 13:33:42 +0300

    using display content dyanmically

--- src/resultComponent.js
+++ src/resultComponent.js
@@ -1,3 +1,137 @@
-function resultComponent(result) {
-  return ``;
+export function resultComponent(result) {
+  return `<div
+          class=""
+        >
+          <div
+            id="searched-word"
+            class="relative font-search-word text-5xl before:absolute before:top-13 before:left-0 before:w-30 before:h-1 before:bg-border"
+          >
+            Plant
+          </div>
+
+          <div class="phonetics flex w-full gap-3 mt-5 italic">
+            <div class="flex gap-2 items-center">
+              <i class="bi bi-volume-up"></i>
+              <span>/plænt/</span>
+            </div>
+            <div class="flex gap-2 items-center">
+              <i class="bi bi-volume-up"></i>
+              <span>/plænt/</span>
+            </div>
+            <div class="flex gap-2 items-center">
+              <i class="bi bi-volume-up"></i>
+              <span>/plænt/</span>
+            </div>
+          </div>
+
+          <div class="meanings w-full flex flex-col gap-5 px-5 mt-5">
+            <div class="parts-of-speech flex flex-col">
+              <div class="part-of-speech italic font-bold">noun</div>
+              <div class="definitions pl-5">
+                <ul class="list-decimal flex flex-col gap-4">
+                  <li>
+                    <div class="definition">
+                      Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                      Obcaecati, saepe sint culpa vitae placeat reprehenderit
+                      odio exercitationem voluptates natus dolore!
+                    </div>
+                    <div class="example pl-2 italic text-border">
+                      <p class="text-header-text">example</p>
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                      Voluptates, delectus.
+                    </div>
+                    <div class="synonyms text-sm">
+                      <p class="italic text-header-text">synonyms</p>
+                      <ul class="flex gap-2">
+                        <li>Lorem, ipsum dolor.</li>
+                        <li>Lorem, ipsum dolor.</li>
+                        <li>Lorem, ipsum dolor.</li>
+                      </ul>
+                    </div>
+                    <div class="antonyms text-sm">
+                      <p class="italic text-header-text">antonyms</p>
+                      <ul class="flex gap-1">
+                        <li>Lorem, ipsum dolor.</li>
+                        <li>Lorem, ipsum dolor.</li>
+                        <li>Lorem, ipsum dolor.</li>
+                      </ul>
+                    </div>
+                  </li>
+                  <li>
+                    <div class="definition">
+                      Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                      Obcaecati, saepe sint culpa vitae placeat reprehenderit
+                      odio exercitationem voluptates natus dolore!
+                    </div>
+                    <div class="example pl-2 italic text-border">
+                      <p class="text-header-text">example</p>
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                      Voluptates, delectus.
+                    </div>
+                    <div class="synonyms text-sm">
+                      <p class="italic text-header-text">synonyms</p>
+                      <ul class="flex gap-2">
+                        <li>Lorem, ipsum dolor.</li>
+                        <li>Lorem, ipsum dolor.</li>
+                        <li>Lorem, ipsum dolor.</li>
+                      </ul>
+                    </div>
+                    <div class="antonyms text-sm">
+                      <p class="italic text-header-text">antonyms</p>
+                      <ul class="flex gap-1">
+                        <li>Lorem, ipsum dolor.</li>
+                        <li>Lorem, ipsum dolor.</li>
+                        <li>Lorem, ipsum dolor.</li>
+                      </ul>
+                    </div>
+                  </li>
+                  <li>
+                    <div class="definition">
+                      Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                      Obcaecati, saepe sint culpa vitae placeat reprehenderit
+                      odio exercitationem voluptates natus dolore!
+                    </div>
+                    <div class="example pl-2 italic text-border">
+                      <p class="text-header-text">example</p>
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                      Voluptates, delectus.
+                    </div>
+                    <div class="synonyms text-sm">
+                      <p class="italic text-header-text">synonyms</p>
+                      <ul class="flex gap-2">
+                        <li>Lorem, ipsum dolor.</li>
+                        <li>Lorem, ipsum dolor.</li>
+                        <li>Lorem, ipsum dolor.</li>
+                      </ul>
+                    </div>
+                    <div class="antonyms text-sm">
+                      <p class="italic text-header-text">antonyms</p>
+                      <ul class="flex gap-1">
+                        <li>Lorem, ipsum dolor.</li>
+                        <li>Lorem, ipsum dolor.</li>
+                        <li>Lorem, ipsum dolor.</li>
+                      </ul>
+                    </div>
+                  </li>
+                </ul>
+              </div>
+              <div class="synonyms text-sm">
+                <p class="italic text-header-text">synonyms</p>
+                <ul class="flex gap-2">
+                  <li>Lorem, ipsum dolor.</li>
+                  <li>Lorem, ipsum dolor.</li>
+                  <li>Lorem, ipsum dolor.</li>
+                </ul>
+              </div>
+              <div class="antonyms text-sm">
+                <p class="italic text-header-text">antonyms</p>
+                <ul class="flex gap-1">
+                  <li>Lorem, ipsum dolor.</li>
+                  <li>Lorem, ipsum dolor.</li>
+                  <li>Lorem, ipsum dolor.</li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>`;
 }
--- src/resultDisplay.js
+++ src/resultDisplay.js
@@ -1,10 +1,15 @@
 /* set up and mount the results from the APi */
 
+import { resultComponent } from "./resultComponent";
+
 const URL = "https://api.dictionaryapi.dev/api/v2/entries/en/";
 
 export default async function showResult(word) {
   /* get the div for result display */
   const resultSection = document.querySelector(".result-section");
+  const resultArea = document.querySelector(".result-area");
+  //clear previous search
+  resultArea.innerHTML = "";
 
   /* Retrieve definition from api */
   try {
@@ -18,6 +23,10 @@ export default async function showResult(word) {
     //perform network calls
     const response = await fetch(url);
     const data = await response.json();
+
+    //Display data to Dom
+    const resultsHtml = data.map((result) => resultComponent(result)).join("");
+    resultArea.innerHTML = resultsHtml;
 
     console.log(data);
   } catch (e) {
